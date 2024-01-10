c05c7a878f2d161d31ac4a8163b650598e0773aa
Cesar <cesarcosta13@outlook.com>
2024-01-10 09:24:47 -0300

    1.0.0

--- pokedex/app.py
+++ pokedex/app.py
@@ -9,6 +9,7 @@ def index():
     poke_image = None
     poke_name = None
     poke_id = None
+    session['poke_id'] = 1
     
     if 'input' in session and session['input'] != None:
         try:
@@ -17,10 +18,8 @@ def index():
             poke_name = data['name']
             poke_id = data['id']
             
-        except requests.exceptions.HTTPError as err:
-            session['input'] = '1'
-            print(session['input'])
-            print(err)
+        except requests.exceptions.JSONDecodeError as err:
+            session['input'] = 1
             poke_image = '../static/MissingNo.png'
             poke_name = 'MissingNo.'
             poke_id = '?????'
@@ -29,9 +28,13 @@ def index():
         data = fetchPokemon('1')
     
     if poke_image == None:
+        session['input'] = 1
         poke_image = '../static/MissingNo.png'
         poke_name = 'MissingNo.'
         poke_id = '?????'
+    
+    if poke_id is not None and poke_id != '?????':
+        session['poke_id'] = poke_id
     
     return render_template('index.html', poke_id = poke_id, poke_name = poke_name, poke_image = poke_image)
 
@@ -41,6 +44,33 @@ def search():
         
     return redirect('/')
 
+@app.route('/next', methods=['POST', ])
+def next():
+    new_id = int(session['poke_id'])
+    
+    if new_id < 649:
+        new_id += 1
+    else:
+        new_id = 1
+    
+    session['input'] = new_id
+    
+    return redirect('/')
+
+@app.route('/prev', methods=['POST', ])
+def prev():
+    new_id = int(session['poke_id'])
+    
+    if new_id > 1:
+        new_id -= 1
+    else:
+        new_id = 649
+    
+    session['input'] = new_id
+    
+    return redirect('/')
+    
+    
 def fetchPokemon(pokemon):
     APIresponse = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
     data = APIresponse.json()    
